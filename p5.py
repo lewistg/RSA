177b02af26f7af8a617e5910aa64855c67097674
Ty Lewis <tyglewis@gamil.com>
2013-10-05 22:05:26 -0600

    Possibly working implemenation.

--- p5.py
+++ p5.py
@@ -1,8 +1,10 @@
+from Crypto.Util import number
 
 def getMultInv(e, modP):
 	"""
-	Finds the multiplicative inverse of e modP using 
-	the table method.
+	Returns tuple of (true, n) where true indicates that the numbers are
+	relatively prime and n which is the multiplicative inverse of e mod p.
+	Otherwise (false, 0) is returned.
 	"""
 	
 	X = 0
@@ -19,15 +21,16 @@ def getMultInv(e, modP):
 	divisorTable[1][X] = 0
 	divisorTable[1][Y] = 1
 	divisorTable[1][D] = e
-	divisorTable[1][K] = modP % e
+	divisorTable[1][K] = modP / e
 
 	while True:
 		x = divisorTable[0][X] - divisorTable[1][K] * divisorTable[1][X]
 		y = divisorTable[0][Y] - divisorTable[1][K] * divisorTable[1][Y]
-		d = int(divisorTable[0][D] % divisorTable[1][D])
+		d = divisorTable[0][D] % divisorTable[1][D]
+		n = divisorTable[0][D] / divisorTable[1][D]
+		assert((x * modP + y * e) == d)
 
 		if d == 0:
-			print "D is " + str(divisorTable[1][D])
 			if divisorTable[1][D] == 1:
 				eInverse = divisorTable[1][Y]
 				if eInverse < 0:
@@ -36,7 +39,7 @@ def getMultInv(e, modP):
 			else:
 				return (False, 0)
 
-		k = int(divisorTable[1][D] / d)
+		k = long(divisorTable[1][D] / d)
 
 		# update our working rows
 		divisorTable[0] = list(divisorTable[1])
@@ -44,9 +47,44 @@ def getMultInv(e, modP):
 		divisorTable[1][Y] = y
 		divisorTable[1][D] = d
 		divisorTable[1][K] = k
+		
+def modExp(g, s, p):
+	"""
+	Calculates s^g mod p
+	"""
+	lowBit = 0x1 
+	result = 1
+	base = g 
+	while s > 0:
+		if (s & lowBit) == 1:
+			result = (result * base) % p
+		s = s >> 1
+		base = (base * base) % p
+		
+	return result
 
 def main():
-	print getMultInv(23, 120)
+	#print getMultInv(23, 120)
+	#print getMultInv(15, 26)
+	e = 65537
+	p = 0
+	q = 0 
+	phiOfN = 0
+	while True:
+		p = number.getStrongPrime(512)
+		q = number.getStrongPrime(512)
+		phiOfN = (p - 1)*(q - 1)
+		if getMultInv(e, phiOfN)[0]:
+			break
+	n = p * q
+
+	multInv = getMultInv(e, phiOfN)
+	assert(multInv[0])
+	d = multInv[1]
+
+	m = 245678
+	print m
+	print modExp(modExp(m, e, n), d, n)
 
 if __name__ == "__main__":
 	main()
